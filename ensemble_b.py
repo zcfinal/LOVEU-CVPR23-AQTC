926d87f8d5d3464fdbbbceb424381627b07b2365
chaozhang <1244382307@qq.com>
2023-06-09 19:24:46 +0800

    ensemble

--- ensemble_b.py
+++ ensemble_b.py
@@ -10,19 +10,39 @@ def softmax(x):
 fileroot = '/data/zclfe/cvpr_comp/LOVEU-CVPR22-AQTC/outputs/cvpr_loveu2023/'
 
 exp_names = [
-    'SSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'SSL_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'noconfibreak_S3Dvideo_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'S3Dvideo_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'splitqa_lr1e-4_bs16_epoch100',
-    'layernorm_kaiming_statepara_timeemb_lr1e-4_bs16_epoch100',
-    'layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
-    'layernorm_kaiming_splitmlp_lr1e-4_bs16_epoch100',
-    'layernorm_kaiming_sentence_lr1e-4_bs16_epoch100',
-    'layernorm_kaiming_lr1e-4_bs16_epoch100',
-    'agglearn_lr1e-4_bs16_epoch100',
-    'baseline_lr1e-4_bs16_epoch100'
+    # 'SSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'SSL_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'noconfibreak_S3Dvideo_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'S3Dvideo_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'splitqa_lr1e-4_bs16_epoch100',
+    # 'layernorm_kaiming_statepara_timeemb_lr1e-4_bs16_epoch100',
+    # 'layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    # 'layernorm_kaiming_splitmlp_lr1e-4_bs16_epoch100',
+    # 'layernorm_kaiming_sentence_lr1e-4_bs16_epoch100',
+    # 'layernorm_kaiming_lr1e-4_bs16_epoch100',
+    # 'agglearn_lr1e-4_bs16_epoch100',
+    # 'baseline_lr1e-4_bs16_epoch100'
+    'moe4_feastep_gate_statedim_32',
+    'moe4_feastep_gate_statedim_64',
+    'moe4_feastep_gate_statedim_128',
+    'moe4_feastep_gate_statedim_256',
+    'moe4_feastep_gate_statedim_512',
+    'moe4_feastep_gate_statedim_1024',
+    'moe4_stepid_gatefeature',
+    'moe4_stepid',
+    'moe4_stepid_gatefeature_statemap',
+    'moe4_feature',
+    'teacherforcelineardecay0_paralearn_idf',
+    'teacherforcelineardecay0_paralearn',
+    'teacherforcelineardecay0_both_paralearn_idf',
+    'teacherforce0.8noconfibreak_facattn_dynamic_video_gatebutton_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    'teacherforcelineardecay',
+    'noconfibreak_facattn_dynamic_video_gatebutton_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    'noconfibreak_dynamic_video_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    'noconfibreak_dynamic_video_gatebutton_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    'noconfibreak_dynamic_tfidf_video_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100',
+    'noconfibreak_videoclip_addtestSSL0.9_layernorm_kaiming_statepara_lr1e-4_bs16_epoch100'
 ]
 
 file_num = len(exp_names)
